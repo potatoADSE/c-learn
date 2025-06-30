b7b6997436f496876dcdf82b761cce4b819bf1be
Matias <156465106+potatoADSE@users.noreply.github.com>
2025-06-30 15:35:24 -0300

    Create calculadora simples.cpp

---
+++ calculadora simples.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cout<<"Digite dois numeros: "<<endl;
+    cin>>a>>b;
+    
+    cout<<"Soma: "<<a+b<<endl;
+    cout<<"Subtracao: "<<a-b<<endl;
+    cout<<"Multiplicacao: "<<a*b<<endl;
+    cout<<"Divisao: "<<a/b;
+}
