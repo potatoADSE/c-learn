8c6152131702101c9bc3c5a03da63b18fb6f4faa
Matias <156465106+potatoADSE@users.noreply.github.com>
2025-07-07 12:55:55 -0300

    Update busca binaria reversa.cpp

--- busca binaria reversa.cpp
+++ busca binaria reversa.cpp
@@ -1,38 +1,30 @@
 #include <iostream>
 #include <chrono>
-#include <ctime>
-#include <iomanip>
 using namespace std;
 
 int main() {
     auto agora = chrono::system_clock::now();
-
-    time_t tempo_segundos = std::chrono::system_clock::to_time_t(agora);
-
     auto duracao = agora.time_since_epoch();
     auto milissegundos_total = chrono::duration_cast<chrono::milliseconds>(duracao).count();
-
-    long long milissegundos_da_segundo_atual = milissegundos_total % 1000;
+    int milissegundos_da_segundo_atual = milissegundos_total % 100+1;
 
     int chute;
-    cout<<"Tente acertar o numero de 1 a 1000 que eu pensei: "<<endl;
-    cin>>chute;
-    if(chute==milissegundos_da_segundo_atual) {
-        cout<<"ACERTOU!"<<endl;
-    }
-    
+
+    cout << "Tente acertar o número de 0 a 100 que eu pensei:" << endl;
+
     do {
-        if(chute<milissegundos_da_segundo_atual) {
-            cout<<"O numero é maior"<<endl;
-            cout<<"Tente novamente: "<<endl;
-            cin>>chute;
+        cout << "Seu chute: ";
+        cin >> chute;
+
+        if (chute < milissegundos_da_segundo_atual) {
+            cout << "O número é maior." << endl;
+        } else if (chute > milissegundos_da_segundo_atual) {
+            cout << "O número é menor." << endl;
+        } else {
+            cout << "ACERTOU!" << endl;
         }
-        else if(chute>milissegundos_da_segundo_atual){
-            cout<<"O numero é menor"<<endl;
-            cout<<"Tente novamente: "<<endl;
-            cin>>chute;
-        }
-    } while (chute!=milissegundos_da_segundo_atual);
+
+    } while (chute != milissegundos_da_segundo_atual);
 
     return 0;
 }
