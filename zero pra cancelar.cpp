2877d6218e86479b62afe51433027f37bd8ea872
Matias <156465106+potatoADSE@users.noreply.github.com>
2025-07-12 19:13:04 -0300

    Create zero pra cancelar.cpp

---
+++ zero pra cancelar.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    int soma=0;
+    int numero;
+    int vezes;
+    cin>>vezes;
+    stack<int> numeros;
+    for (int i=1;i<=vezes;i++) {
+        cin>>numero;
+        if (numero!=0) {
+            numeros.push(numero);
+        }
+        else if (!numeros.empty()&&numero==0) {
+            numeros.pop();
+        }
+    }
+    while (!numeros.empty()) {
+        numeros.top()+soma;
+        numeros.pop();
+    }
+    cout<<soma;
+    
+    return 0;
+}
